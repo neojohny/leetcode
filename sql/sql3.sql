4ee95e29ef13ca7dc09194ec2d9f19cf479064c7
Zhao, John <yzhao644@gatech.edu>
2020-12-30 18:07:51 -0800

    sql

--- sql/sql3.sql
+++ sql/sql3.sql
@@ -16,12 +16,11 @@ inner join
 (select question_id,
 sum(case when event = 'answered' then 1 else 0 end)/sum(case when event = 'imp' then 1 else 0 end) as con
 from survey_log
-where id in (select question_id from survey_log 
+where question_id in (select question_id from survey_log 
 where event = 'imp'
-and question_id = someid
 group by 1
-having count(1)>10 and question_id
-)
+having count(1)>10
+) and user_id in (select user_id from survey_log  where question_id = someid  and event = 'answer')
 group by 1
 order by 2 desc
 ) q
@@ -29,3 +28,17 @@ on u.question_id = q.question_id
 where question_id not in (someid)
 order by con desc
 limit 1
+
+
+
+-- 14 
+select sum( spend )/count(1)
+from (
+select advertiser_id,case when sum(price) >0 then 1 else 0 end as spend
+from adv_info v left join ad_info i on v.ad_id = v.ad_id
+group by 1) result
+
+select advertiser_id,sum(price),sum(spend)
+from adv_info v left join ad_info i on v.ad_id = v.ad_id
+group by 1
+
