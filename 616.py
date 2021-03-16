37592c3e549c3e6d46ddc3dc2cb4ec969b5fea95
Zhao, John <yzhao644@gatech.edu>
2021-03-16 16:33:33 -0700

    616

---
+++ 616.py
@@ -0,0 +1,38 @@
+from collections import deque
+class Solution:
+    """
+    @param: numCourses: a total of n courses
+    @param: prerequisites: a list of prerequisite pairs
+    @return: the course order
+    """
+    def findOrder(self, numCourses, prerequisites):
+        # write your code here
+        if not numCourses:
+            return []
+        nodeindegree = [0 for _ in range(numCourses)]
+        nodeneighbour = [[] for _ in range(numCourses)]
+
+        for c,pre in prerequisites:
+            nodeindegree[pre] += 1
+            nodeneighbour[c].append(pre)
+
+        startcourse = []
+        for i in range(numCourses):
+            if not nodeindegree[i]:
+                startcourse.append(i)
+        q = deque(startcourse)
+        result = []
+        while q:
+            head = q.popleft()
+            result.append(head)
+            nei = nodeneighbour[head]
+            for n in nei:
+                nodeindegree[n] -= 1
+                if nodeindegree[n] == 0:
+                    q.append(n)
+        
+        if len(result) == numCourses:
+            return result[::-1]
+        
+        else:
+            return []
