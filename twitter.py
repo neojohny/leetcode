c28e21913e47da1bdadd57772dcc0485d1c98149
Zhao, John <yzhao644@gatech.edu>
2021-05-22 15:02:38 -0700

    twitter interview

---
+++ twitter.py
@@ -0,0 +1,15 @@
+#https://www.techiedelight.com/smallest-window-sorting-which-make-array-sorted/
+
+"""
+Find the smallest window in an array sorting which will make the entire array sorted
+given an integer array, find the smallest window sorting which will make the entire 
+array sorted in increaseing order
+
+Input:  { 1, 2, 3, 7, 5, 6, 4, 8 }
+Output: Sort the array from index 3 to 6
+ 
+ 
+Input:  { 1, 3, 2, 7, 5, 6, 4, 8 }
+Output: Sort the array from index 1 to 6
+"""
+
