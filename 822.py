c39ffb191125bf78ee191b94d1263075a01bce91
John Zhao <yi.zhao@acqu.co>
2022-08-23 22:35:02 -0700

    not my solution

--- 822.py
+++ 822.py
@@ -1,1 +1,5 @@
 # this is a default code for 822
+class Solution:
+    def flipgame(self, fronts: List[int], backs: List[int]) -> int:
+        bad=set([a for a,b in zip(fronts,backs) if a==b])
+        return min([el for el in fronts+backs if el not in bad],default=0)
